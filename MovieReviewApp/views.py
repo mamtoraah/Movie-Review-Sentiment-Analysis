4bdfb4079b0e70a1d6dc0bc393756d9566c96b17
hemal <hemalmamtora1@gmail.com>
2019-02-02 15:19:18 +0530

    data from IMDB api fetched and able to display

--- MovieReviewApp/views.py
+++ MovieReviewApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+import requests
+from MovieReviewApp.key import apikey
 
 # Create your views here.
 def home_view(request):
-  return render(request, 'MovieReviewApp/home.html')
+  key = apikey
+  url = 'http://www.omdbapi.com/?i=tt3896198&apikey=' + apikey
+  response = requests.get(url)
+  data = response.json()
+  return render(request, 'MovieReviewApp/home.html', {'data': data})
