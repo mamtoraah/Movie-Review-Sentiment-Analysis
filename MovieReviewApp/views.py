d44fcf7bc23a9c7725688578ff9ecc3cca699905
Jinay <jinayparekh98@gmail.com>
2019-02-03 18:31:26 +0530

    Retrieval of reviews from tmdb api

--- MovieReviewApp/views.py
+++ MovieReviewApp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 import requests
+import tmdbsimple as tmdb
 from MovieReviewApp.key import apikey
 
 # Create your views here.
+# def home_view(request):
+#   key = apikey
+#   url = 'http://www.omdbapi.com/?i=tt3896198&apikey=' + apikey
+#   response = requests.get(url)
+#   data = response.json()
+#   return render(request, 'MovieReviewApp/home.html', {'data': data})
 def home_view(request):
-  key = apikey
-  url = 'http://www.omdbapi.com/?i=tt3896198&apikey=' + apikey
-  response = requests.get(url)
-  data = response.json()
-  return render(request, 'MovieReviewApp/home.html', {'data': data})
+  tmdb.API_KEY ='key lelena mere paas se'
+  movie = tmdb.Movies(603)
+  print(movie)
+  response=movie.info()
+  response1=movie.reviews()
+  print(response1)
+  return render(request, 'MovieReviewApp/home.html', {'data': response1})
