a80ef1e7268a0650ab797b1983156988b6511e4c
Jinay <jinayparekh98@gmail.com>
2019-03-16 17:07:38 +0530

    UI changes

--- MovieReviewApp/urls.py
+++ MovieReviewApp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls import url
 #from django.contrib.auth.views import login, logout
 
 app_name = 'MovieReviewApp'
 urlpatterns = [
   path('', views.home_view, name='home'),
-  path('graph/', views.graph_view, name='graph')
+  url(r'^graph/$',views.graph_view,name='graph'),
+  #path('/graph', views.graph_view, name='graph')
+
   #path('/<str:>')
 ]
