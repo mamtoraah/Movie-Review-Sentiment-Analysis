adf355fc718885933261d30f90f30a357bb60339
hemal <hemalmamtora1@gmail.com>
2019-03-17 20:28:01 +0530

    able to run sentiment analysis for tmdb as well as omdb reviews

--- MovieReviewApp/views.py
+++ MovieReviewApp/views.py
@@ -17,137 +17,188 @@ Shawshank redemption: avg:  0.245180390140919  count:  25
 '''
 #import scrapy
 
-REPLACE_NO_SPACE = re.compile("(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
-REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+REPLACE_NO_SPACE = re.compile(
+    "(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
+REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)|(\\r)|(\\n)")
+
 
 def preprocess_reviews(reviews):
   reviews = [REPLACE_NO_SPACE.sub("", line.lower()) for line in reviews]
   reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
   return ''.join(reviews)
 
-# def scrape():
-#   temp = scrapy.fetch("https://www.reddit.com/r/gameofthrones/")
-#   print(temp)
+
+def scrape():
+  temp = scrapy.fetch("https://www.reddit.com/r/gameofthrones/")
+  print(temp)
 
 # Create your views here.
-# def home_view(request):
-#   data = ''
-#   if request.method ==  'POST':
-#     key = apikey
-#     movie_name = request.POST.get('search_movie')
-#
-#     filename = os.path.dirname(os.path.realpath(__file__)) + '/data.json'
-#     with open(filename) as json_file:
-#       temp = json.load(json_file)
-#
-#     t = movie_name.replace(" ", "").lower()
-#     for i in temp['movie']:
-#       if (i['movie_name'] == t):
-#         return render(request, 'MovieReviewApp/home.html', {'data':i['sentiment']})
-#
-#     movie_str ='&s=' + movie_name
-#     #http://www.omdbapi.com/?apikey=404e7523&s=The Shawshank Redemption
-#     url = 'http://www.omdbapi.com/?apikey=' + apikey + movie_str
-#     response = requests.get(url)
-#     data = response.json()
-#     id = data['Search'][0]['imdbID']
-#     print("IMDBID:", id)
-#     page = requests.get('https://www.imdb.com/title/'+id+'/reviews?ref_=tt_urv')
-#     soup = BeautifulSoup(page.text, 'html.parser')
-#     #print(soup.prettify())
-#     dtags = soup.find_all("div", { "class" : "text show-more__control" })
-#     #print("d tags are",dtags)
-#
-#     listofreviews=[]
-#     cleanedreviews=[]
-#     preprocessedreviews = []
-#     for d in dtags:
-#       listofreviews.append(d.text.strip())
-#
-#     #print(listofreviews)
-#     for str in listofreviews:
-#       startindex=str.find('Favorite films:')
-#       cleanedstr=str[0:startindex]
-#       cleanedreviews.append(cleanedstr)
-#       #print("Start is",startindex)
-#     sentiment = 0.0
-#     count = 0
-#     for s in cleanedreviews:
-#       preprocessedreviews.append(preprocess_reviews(s))
-#       count += 1
-#       blob_object = TextBlob(s)
-#       sentiment += blob_object.sentiment.polarity
-#       print(sentiment)
-#       data = sentiment/count
-#     print("avg: ", data, " count: ", count)
-#
-#     #preprocessedreviews is a list of reviews on which the sentiment analysis is applied.
-#     #save the preprocessed reviews in a file named : data.json
-#     #structure : {moviename : {review : [reviews], sentiment : value}
-#     movie = movie_name.replace(" ", "").lower()
-#     pydict = {'movie_name': movie, 'review':preprocessedreviews, 'sentiment': data}
-#
-#     temp['movie'].append(pydict)
-#
-#     with open(filename, 'w') as f:
-#       json.dump(temp, f)
-#
-#     sourcelist=[]
-#     ratelist=[]
-#     urlrating = 'http://www.omdbapi.com/?apikey='+apikey+'&i='+id+'&plot=short&r=json&tomatoes=true'
-#     responserating = requests.get(urlrating)
-#     datarating = responserating.json()
-#     print("datarating is",datarating)
-#     ratingarray=datarating['Ratings']
-#     for x in ratingarray:
-#       source=x['Source']
-#       rating=x['Value']
-#       sourcelist.append(source)
-#       ratelist.append(rating)
-#     print("Source list is ",sourcelist)
-#     print("Rating is ",ratelist)
-#
-#
-#
-#   return render(request, 'MovieReviewApp/home.html', {'data':data})
-
 
 
 def graph_view(request):
+  values = {
+              'Sentiment Rating from TMDB API': 10,
+              'Sentiment Rating from Web Scraper': 8,
+              'Actual Rating': 6.2,
+              'Proposed Rating': 6.8
+          }
   data = ''
-  if request.method ==  'POST':
-      key = apikey
-      movie_name = request.POST.get('search_movie')
-      print("Movie name is =",movie_name)
-      filename = os.path.dirname(os.path.realpath(__file__)) + '/data.json'
-      with open(filename) as json_file:
-        temp = json.load(json_file)
+  if request.method == 'POST':
+    key = apikey
+    movie_name = request.POST.get('search_movie')
 
-      t = movie_name.replace(" ", "").lower()
-      for i in temp['movie']:
-        if (i['movie_name'] == t):
-          return render(request, 'MovieReviewApp/graph.html', {'data':i['sentiment']})
+    filename = os.path.dirname(os.path.realpath(__file__)) + '/data.json'
+    with open(filename) as json_file:
+      temp = json.load(json_file)
 
-      movie_str ='&s=' + movie_name
-      print("Movie name  is :",movie_str)
-  print('hello this is from graph view')
-  values={'Sentiment Rating from TMDB API':8.5,'Sentiment Rating from Web Scraper':7.5,'Actual Rating':6.2,'Proposed Rating':6.8}
+    t = movie_name.replace(" ", "").lower()
+    for i in temp['movie']:
+      if (i['movie_name'] == t):
+        values = {
+            'Sentiment Rating from TMDB API': i['tmdb_rating'],
+            'Sentiment Rating from Web Scraper': i['sentiment'],
+            'Actual Rating': 6.2,
+            'Proposed Rating': 6.8
+        }
+        return render(request, 'MovieReviewApp/home.html', {'data': values})
+    movie_str = '&s=' + movie_name
+    # http://www.omdbapi.com/?apikey=404e7523&s=The Shawshank Redemption
+    url = 'http://www.omdbapi.com/?apikey=' + apikey + movie_str
+    response = requests.get(url)
+    data = response.json()
+    id = data['Search'][0]['imdbID']
+    print("IMDBID:", id)
+    page = requests.get('https://www.imdb.com/title/' +
+                        id+'/reviews?ref_=tt_urv')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    # print(soup.prettify())
+    dtags = soup.find_all("div", {"class": "text show-more__control"})
+    #print("d tags are",dtags)
+
+    listofreviews = []
+    cleanedreviews = []
+    preprocessedreviews = []
+    for d in dtags:
+      listofreviews.append(d.text.strip())
+
+    # print(listofreviews)
+    for str in listofreviews:
+      startindex = str.find('Favorite films:')
+      cleanedstr = str[0:startindex]
+      cleanedreviews.append(cleanedstr)
+      #print("Start is",startindex)
+    sentiment = 0.0
+    count = 0
+    for s in cleanedreviews:
+      preprocessedreviews.append(preprocess_reviews(s))
+      count += 1
+      blob_object = TextBlob(s)
+      sentiment += blob_object.sentiment.polarity
+      print(sentiment)
+      data = sentiment/count
+    print("avg: ", data, " count: ", count)
+
+    # preprocessedreviews is a list of reviews on which the sentiment analysis 
+    # is applied.
+    # save the preprocessed reviews in a file named : data.json
+    # structure : 
+    # {moviename : {review : [reviews], sentiment : value, tmdb_rating}
+    movie = movie_name.replace(" ", "").lower()
+    tmdb.API_KEY = '521b67c3f5ed3c1369247d6bf7592c00'
+    search = tmdb.Search()
+    #avgvote = search.movie(query=movie_name)['results'][0]['vote_average']
+    #print("Avg vote is ", avgvote)
+    #tmdb_rating = avgvote
+    tmdb_id = search.movie(query=movie_name)['results'][0]['id']
+    _movie = tmdb.Movies(id)
+    response1 = _movie.reviews()
+    tmdb_review = response1['results'][0]['content']
+    print(tmdb_review.lower())
+    tmdb_preprocessed = preprocess_reviews(tmdb_review)
+    print(tmdb_preprocessed)
+    tmdb_blob_object = TextBlob(tmdb_preprocessed)
+    tmdb_sentiment = blob_object.sentiment.polarity
+    print(tmdb_sentiment)
+    
+    #pydict to store in the database: data.json
+    pydict = {
+        'movie_name': movie,
+        'review': preprocessedreviews,
+        'sentiment': data,
+        'tmdb_review' : tmdb_preprocessed,
+        'tmdb_rating': tmdb_sentiment
+    }
+
+    temp['movie'].append(pydict)
+
+    with open(filename, 'w') as f:
+      json.dump(temp, f)
+
+    # sourcelist = []
+    # ratelist = []
+    # urlrating = 'http://www.omdbapi.com/?apikey=' + \
+    #     apikey+'&i='+id+'&plot=short&r=json&tomatoes=true'
+    # responserating = requests.get(urlrating)
+    # datarating = responserating.json()
+    # print("datarating is", datarating)
+    # ratingarray = datarating['Ratings']
+    # for x in ratingarray:
+    #   source = x['Source']
+    #   rating = x['Value']
+    #   sourcelist.append(source)
+    #   ratelist.append(rating)
+    # print("Source list is ", sourcelist)
+    # print("Rating is ", ratelist)
+
+    #values to display
+    values = {
+              'Sentiment Rating from TMDB API': tmdb_sentiment,
+              'Sentiment Rating from Web Scraper': data,
+              'Actual Rating': 6.2,
+              'Proposed Rating': 6.8
+          }
   return render(request, 'MovieReviewApp/graph.html', {'data': values})
 
 
-def home_view(request):
- tmdb.API_KEY ='784b4dff6c62ccbe711abb6b8163979f'
- search = tmdb.Search()
- avgvote=search.movie(query='Dangal')['results'][0]['vote_average']
- print("Avg vote is ",avgvote)
- popularity = search.movie(query='Dangal')['results'][0]['popularity']
- print("popularity is ",popularity)
- id = search.movie(query='Dangal')['results'][0]['id']
- print("The id is",id)
- movie = tmdb.Movies(id)
+# def graph_view(request):
+#   data = ''
+#   if request.method == 'POST':
+#     key = apikey
+#     movie_name = request.POST.get('search_movie')
+#     print("Movie name is =", movie_name)
+#     filename = os.path.dirname(os.path.realpath(__file__)) + '/data.json'
+#     with open(filename) as json_file:
+#       temp = json.load(json_file)
 
- #print(movie.reviews())
- response1=movie.reviews()
- data = response1['results'][0]['content']
- print(data)
- return render(request, 'MovieReviewApp/home.html', {'data': 1})
+#     t = movie_name.replace(" ", "").lower()
+#     for i in temp['movie']:
+#       if (i['movie_name'] == t):
+#         return render(request, 'MovieReviewApp/graph.html', {'data': i['sentiment']})
+
+#     movie_str = '&s=' + movie_name
+#     print("Movie name  is :", movie_str)
+#   print('hello this is from graph view')
+#   values = {'Sentiment Rating from TMDB API': 8.5,
+#             'Sentiment Rating from Web Scraper': 7.5, 'Actual Rating': 6.2, 'Proposed Rating': 6.8}
+#   return render(request, 'MovieReviewApp/graph.html', {'data': values})
+
+# hemal's key : 521b67c3f5ed3c1369247d6bf7592c00
+# jinay's key : 784b4dff6c62ccbe711abb6b8163979f
+
+
+def home_view(request):
+  # tmdb.API_KEY = '521b67c3f5ed3c1369247d6bf7592c00'
+  # search = tmdb.Search()
+  # avgvote = search.movie(query='Dangal')['results'][0]['vote_average']
+  # print("Avg vote is ", avgvote)
+  # popularity = search.movie(query='Dangal')['results'][0]['popularity']
+  # print("popularity is ", popularity)
+  # id = search.movie(query='Dangal')['results'][0]['id']
+  # print("The id is", id)
+  # movie = tmdb.Movies(id)
+
+  # # print(movie.reviews())
+  # response1 = movie.reviews()
+  # data = response1['results'][0]['content']
+  #print(data)
+  return render(request, 'MovieReviewApp/home.html', {'data': 1})
