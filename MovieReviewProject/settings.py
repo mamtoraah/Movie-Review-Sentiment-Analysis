cf4722f57729b35bdf67fb23d840289c9ffa8960
Jinay <jinayparekh98@gmail.com>
2019-03-24 14:17:32 +0530

    Carousel view

--- MovieReviewProject/settings.py
+++ MovieReviewProject/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/MovieReviewApp/static/MovieReviewApp'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '/MovieReviewApp/static/MovieReviewApp'),
+# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
